95befffa7987d9755104a57b1c5624a5f53394c6
ColinLai0311 <colin02718@gmail.com>
2023-01-03 15:08:08 +0800

    20230103
    add index.jsp

--- src/main/java/life4fun/servlet/ProductServlet.java
+++ src/main/java/life4fun/servlet/ProductServlet.java
@@ -1,41 +1,73 @@
 package life4fun.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import life4fun.entity.Product;
+import life4fun.exception.VGBException;
+import life4fun.service.MemberService;
+import life4fun.service.ProductService;
+import life4fun.service.StreetNameService;
+import life4fun.service.impl.MemberServiceImpl;
+import life4fun.service.impl.StreetNameServiceImpl;
+import life4fun.utils.ApplicationUtils;
+import life4fun.utils.ConstantUtils;
+
 /**
  * Servlet implementation class ProductServlet
  */
 @WebServlet("/ProductServlet")
-public class ProductServlet extends _Servlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProductServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+public class ProductServlet extends HttpServlet {
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public static final long serialVersionUID = 1L;
+	public static final String JSP_SOURCE = "/jsp/product/";
+	public static final String JS_SOURCE = "/js/product/";
+	public static final String STATIC_SOURCE = "static";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute(ApplicationUtils.JS_SOURCE_KEY, JS_SOURCE);
+		request.setAttribute(ApplicationUtils.STATIC_SOURCE_KEY, STATIC_SOURCE);
+		request.setCharacterEncoding("UTF-8");
+		String method = request.getParameter("method") == null ? "getAllProducts" : request.getParameter("method");
+		System.out.println("method -> " + method);
+		switch (method) {
+		case "getAllProducts":
+			try {
+				getAllProducts(request, response);
+			} catch (ServletException | IOException | VGBException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		
+		
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
+
+	public void getAllProducts(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, VGBException {
+		
+		ProductService service = new ProductService();
+		List<Product> list = null;
+		
+			list = service.getAllProducts();
+
+		request.setAttribute("product",list);
+		System.out.println("JSP_SOURCE"+JSP_SOURCE);
+		request.getRequestDispatcher(JSP_SOURCE+"Product.jsp").forward(request, response);
+}
+	
+
 
 }
