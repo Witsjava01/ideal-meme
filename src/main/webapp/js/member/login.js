ec100947ed1c82c60175cd3e3fc633aef16cd0da
esther <esther910212@gmail.com>
2023-01-16 09:55:52 +0800

    member

---
+++ src/main/webapp/js/member/login.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	$('#register').on("click", function() {
+		window.location.assign(webApplicationPath + "/MemberServlet?method=register");
+	});
+	$('#loginButton').on("click", function() {
+		var inputData = {};
+		if ($('#account').val() === "" || $('#password').val() === "" ||
+			$('#account').val() === null || $('#password').val() === null) {
+			alert('請輸入帳號密碼!')
+			return false;
+		} else {
+			$('input[class="input-text"]').each(function() {
+				inputData[$(this).attr('id')] = $(this).val();
+			});
+			$.ajax({
+				url: webApplicationPath + "/MemberServlet?method=findMember",
+				type: "POST",
+				data: inputData,
+				dataType: "json",
+				contentType: "application/x-www-form-urlencoded;charset=utf-8",
+				success: function(response) {
+					if (response.result) {
+						console.log(response);
+						alert("登入成功");
+						$("#SignIn_A").text("會員中心");
+						window.location.assign(webApplicationPath + "/MemberServlet?method=member");
+					} else {
+						alert(response.msg);
+					}
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					alert("失敗");
+					/*弹出jqXHR对象的信息*/
+					console.log(jqXHR.responseText);
+					console.log(jqXHR.status);
+					console.log(jqXHR.readyState);
+					console.log(jqXHR.statusText);
+					/*弹出其他两个参数的信息*/
+					console.log(textStatus);
+					console.log(errorThrown);
+				}
+			});
+		}
+
+	});
+
+
+});
